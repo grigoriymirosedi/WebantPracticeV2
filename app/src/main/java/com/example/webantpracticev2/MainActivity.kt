505ef612cd84ee21cd7026fba37e0f90a9cd0825
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-06-05 00:30:15 +0300

    added animated splashscreen
    - added background color for splashscreen
    - added vector image
    - changing in themes
    - changed manifest file

--- app/src/main/java/com/example/webantpracticev2/MainActivity.kt
+++ app/src/main/java/com/example/webantpracticev2/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.webantpracticev2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.window.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                true
+            }
+        }
         setContentView(R.layout.activity_main)
     }
 }
