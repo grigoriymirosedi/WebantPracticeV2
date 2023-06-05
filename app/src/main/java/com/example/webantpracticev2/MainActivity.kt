af9ae2e1778e9c387cc1c3e9daec93f40c427a4b
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-06-05 15:58:31 +0300

    added MaterialToolbar
    build gradle lib verisons updated

--- app/src/main/java/com/example/webantpracticev2/MainActivity.kt
+++ app/src/main/java/com/example/webantpracticev2/MainActivity.kt
@@ -4,16 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.window.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.webantpracticev2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
-            setKeepOnScreenCondition {
-                //TODO: replace true with Handler?
-                true
-            }
+//            setKeepOnScreenCondition {
+//                //TODO: replace true with Handler?
+//                true
+//            }
         }
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
