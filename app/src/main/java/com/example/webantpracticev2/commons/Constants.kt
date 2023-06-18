93cc6b8bc582c6d91dd87447006724859d9135f8
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-06-18 13:43:53 +0300

    fixed response queries
    added query for pagination
    added interceptor for debugging
    fixed base url
    structured dependencies in gradle

--- app/src/main/java/com/example/webantpracticev2/commons/Constants.kt
+++ app/src/main/java/com/example/webantpracticev2/commons/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.webantpracticev2.commons
 
 object Constants {
-    const val BASE_URL="https://gorest.co.in/public-api/posts/"
+    const val BASE_URL="https://gorest.co.in/"
 }
--- app/src/main/java/com/example/webantpracticev2/data/remote/api/PostsApi.kt
+++ app/src/main/java/com/example/webantpracticev2/data/remote/api/PostsApi.kt
@@ -3,8 +3,12 @@ package com.example.webantpracticev2.data.remote.api
 import com.example.webantpracticev2.data.remote.dto.PostListDto
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PostsApi {
-    @GET("?page=1&per_page=20")
-    fun getPostsList(): Call<PostListDto>
+    @GET("public-api/posts")
+    fun getPostsList(
+        @Query("page")
+        pageNumber: Int = 1
+    ): Call<PostListDto> // Replace with Response<PostListDto> ???
 }
